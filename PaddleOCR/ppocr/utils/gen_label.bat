96d4085a1295c2f6daa93da1e94db8a00964f58c
Nam <nguyentoney797@gmail.com>
2022-04-19 13:56:42 +0700

    first

---
+++ PaddleOCR/ppocr/utils/gen_label.bat
@@ -0,0 +1,2 @@
+python gen_label.py --mode="det" --root_path="icdar_c4_train_imgs/" --input_path="ch4_training_localization_transcription_gt" --output_label="train_icdar2015_label.txt"
+PAUSE
---
+++ PaddleOCR/train.bat
@@ -0,0 +1,2 @@
+python tools/train.py -c configs/det/det_mv3_db.yml -o Global.pretrain_weights=./pretrain_models/MobileNetV3_large_x0_5_pretrained/
+PAUSE
